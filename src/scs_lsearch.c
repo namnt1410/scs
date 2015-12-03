3307bb7d9906d88382e38742f96024ab20e24da9
namnt1410 <namnt.1410@gmail.com>
2015-12-03 20:31:59 +0700

    Fix scs lsearch

--- src/scs_lsearch.c
+++ src/scs_lsearch.c
@@ -8,16 +8,9 @@
 int scs_lsearch (SequenceList list, int *super) {
   Solution *sol;
   SolutionNode *node;
-  int i, pos = -1; 
 
   sol = lsearch(list); 
   int val = ls_evaluate (sol, 0, sol->sol_len - 1, super);
-
-  /*node = sol->first;
-  while (node) {
-    if (node->pos < pos) printf("valid: 0\n");   
-    pos = node->pos; node = node->next;
-  }*/
 
   printf("val: %d\n", val);
   printf("check: %d\n", check_common_supersequence(list, super, val));
