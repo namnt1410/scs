0d69a90a6ebf13d6b809b1466bb96c41bcde72dc
namnt.1410 <namnt.1410@gmail.com>
2016-03-07 22:07:40 +0700

    Create DR algorithm

--- project-conf.h
+++ project-conf.h
@@ -1,10 +1,10 @@
 #ifndef __PROJECT_CONF_H__
 #define __PROJECT_CONF_H__
 
-#define MM_CONF_MAJORITY def_majority
+#define MM_CONF_MAJORITY heuristics_majority_H1
 
 #define LC_CONF_TYPE LC_TYPE_SHIFT	
 
-#define LS_CONF_MAX_LOOP_COUNT				50
+#define LS_CONF_MAX_LOOP_COUNT				20
 
 #endif
---
+++ src/scs_deposition_reduction.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "scs.h"
+
+int scs_deposition_reduction(SequenceList list, int *super, int (*scs)(SequenceList , int *)) {
+  Sequence* seq;
+  SequenceList left;  
+  int temp[MAX_LEN], better;
+  int len, left_len;
+  int i, j;
+
+  len = scs (list, super);
+
+  left = NULL; seq = list;
+  while (seq) {
+    add_sequence (&left, seq->seq, seq->len);
+    seq = seq->next;
+  }
+
+  do {
+    better = 0; 
+    seq = left;
+    while (seq) {
+      seq->len = 0; seq = seq->next;
+    } 
+
+    for (i = 1; i < len; i++) {
+      seq = left; j = 0;
+      while (seq) {
+        if (seq->seq[seq->len] == super[i - 1]) {
+          seq->len++; 
+        }
+        seq = seq->next; j++;
+      } 
+
+      left_len = scs_mmerge (left, temp);
+      if (left_len < i) {
+        better = 1;
+        memcpy (temp + left_len, super + i, (len - i) * sizeof(int));
+        len = left_len + len - i;
+        memcpy (super, temp, len * sizeof(int));
+        break;          
+      }
+    }
+  } while (better);
+  
+  return len;
+}
