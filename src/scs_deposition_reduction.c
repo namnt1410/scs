fe57cf58f9939a47f3aa00a0dc3eb00abcf31769
namnt.1410 <namnt.1410@gmail.com>
2016-03-21 14:36:16 +0700

    Finish update DR algorithm

--- src/scs_deposition_reduction.c
+++ src/scs_deposition_reduction.c
@@ -6,42 +6,45 @@
 #include "scs.h"
 
 int scs_deposition_reduction(SequenceList list, int *super, int (*scs)(SequenceList , int *)) {
-  Sequence* seq;
-  SequenceList left;  
-  int temp[MAX_LEN], better;
-  int len, left_len;
+  Sequence *seq, *temp;
+  SequenceList tmp;  
+  int left[MAX_LEN], right[MAX_LEN], better;
+  int len, left_len, right_len;
   int i, j;
 
   len = scs (list, super);
 
-  left = NULL; seq = list;
+  tmp = NULL; seq = list;
   while (seq) {
-    add_sequence (&left, seq->seq, seq->len);
+    add_sequence (&tmp, seq->seq, seq->len);
     seq = seq->next;
   }
 
   do {
     better = 0; 
-    seq = left;
+    seq = tmp;
     while (seq) {
       seq->len = 0; seq = seq->next;
     } 
 
     for (i = 1; i < len; i++) {
-      seq = left; j = 0;
+      right_len = len - i;
+      memcpy (right, super + i, right_len * sizeof(int));
+ 
+      temp = tmp; seq = list; j = 0;
       while (seq) {
-        if (seq->seq[seq->len] == super[i - 1]) {
-          seq->len++; 
+        if (seq->seq[temp->len] == super[i - 1]) {
+          if (!check_supersequence (seq->seq + temp->len, seq->len - temp->len, right, right_len)) temp->len++; 
         }
-        seq = seq->next; j++;
+        seq = seq->next; temp = temp->next; j++;
       } 
 
-      left_len = scs_mmerge_H1 (left, temp);
+      left_len = scs_mmerge_H1 (tmp, left);
       if (left_len < i) {
         better = 1;
-        memcpy (temp + left_len, super + i, (len - i) * sizeof(int));
-        len = left_len + len - i;
-        memcpy (super, temp, len * sizeof(int));
+        memcpy (super + left_len, right, right_len * sizeof(int));
+        len = left_len + right_len;
+        memcpy (super, left, left_len * sizeof(int));
         break;          
       }
     }
